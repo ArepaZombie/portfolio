147615499bffe5fffcf4e0614a03339e056b5633
ArepaZombie <jorgetorresmora99@gmail.com>
2022-10-06 23:43:18 -0500

    Make a filtrer for the skills

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*LAS ANIMACIONES*/
 let screensize = window.innerHeight;
 let sections = ["about","skills","projects","contact"].map((section)=>document.getElementById(section))
 
@@ -23,4 +24,39 @@ function show_hero(){
 }
 
 window.addEventListener('scroll',() => sections.map(section => effect(section)));
-show_hero()
+show_hero() 
+
+/*PARA EL FILTRO DE SKILLS*/
+let pSkills = Array.from(document.getElementById("skills-container").children);
+let arrSkills = []
+pSkills.map(skill=>Array.from(skill.children).map(child => arrSkills.push(child)))
+
+let skillNav = Array.from(document.querySelector('#skills-nav ul').children)
+
+function filtrar(categoria){
+  skillNav.map(nav=>{
+    if(nav.id==categoria){
+      nav.classList.add('selected')
+    }
+    else{
+      nav.classList.remove('selected')
+    }
+  })
+
+  arrSkills.map(skill => {
+    if (categoria=='a'){
+      skill.classList.remove('skill-out')
+    }
+    else if (skill.classList[0]!=categoria){
+      skill.classList.add('skill-out')
+    }
+    else{
+      skill.classList.remove('skill-out')
+    }
+  })
+}
+
+document.getElementById('a').setAttribute('onclick','filtrar("a")')
+document.getElementById('tool').setAttribute('onclick','filtrar("tool")')
+document.getElementById('lang').setAttribute('onclick','filtrar("lang")')
+document.getElementById('soft').setAttribute('onclick','filtrar("soft")')
